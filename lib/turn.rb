43f31ad39a766c1574645bdaaf5a36eea1442a05
Lucas Souza <souza.lucas01@gmail.com>
2020-03-12 03:18:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,8 +39,7 @@ def turn(board)
   user_input = gets .strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
-    move(board, converted_input, user_input = "X")
-
+  
   end
 
 end

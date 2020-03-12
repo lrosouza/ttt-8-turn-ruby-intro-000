037468bcd5c10b1f79ab937be8d53fa08ef95adf
Lucas Souza <souza.lucas01@gmail.com>
2020-03-12 01:46:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,7 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets .strip
   input_to_index(user_input)
-  if input_to_index(board) > 9
+  if input_to_index(user_input) > 9
     puts "Vai de novo!"
   end
 
